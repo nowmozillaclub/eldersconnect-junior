5e2c0ccd29142bfc2e00c08e02ca66ee2691e059
urmilshroff <urmilshroff@gmail.com>
2019-09-24 11:50:04 +0530

    Finished Google signin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ec_junior/pages/home_page.dart';
+import 'package:ec_junior/pages/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MyLoginPage(),
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,40 +1,11 @@
+import 'package:ec_junior/pages/home_page.dart';
+import 'package:ec_junior/services/login_helper.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 
 class MyLoginPage extends StatefulWidget {
   @override
   _MyLoginPageState createState() => _MyLoginPageState();
-}
-
-final FirebaseAuth _auth = FirebaseAuth.instance;
-final GoogleSignIn _googleSignIn = GoogleSignIn();
-Future<String> signInWithGoogle() async {
-  final GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
-
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
-
-  final FirebaseUser user = await _auth.signInWithCredential(credential);
-
-  assert(!user.isAnonymous);
-  assert(await user.getIdToken() != null);
-
-  final FirebaseUser currentUser = await _auth.currentUser();
-  assert(user.uid == currentUser.uid);
-
-  return 'Login success: $user';
-}
-
-void signOutGoogle() async {
-  await _googleSignIn.signOut();
-
-  print('User logged out');
 }
 
 class _MyLoginPageState extends State<MyLoginPage> {
@@ -46,9 +17,24 @@ class _MyLoginPageState extends State<MyLoginPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text('Welcome!'),
+            Text(
+              'Welcome!',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
             GoogleSignInButton(
-              onPressed: () {},
+              onPressed: () {
+                signInWithGoogle().whenComplete(() {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return MyHomePage();
+                      },
+                    ),
+                  );
+                });
+              },
               darkMode: true,
             ),
           ],
