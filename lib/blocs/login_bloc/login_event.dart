1fec2f3fa804f653899a89a8be527df6f167def6
urmilshroff <urmilshroff@gmail.com>
2020-02-19 11:36:28 +0530

    Revert "Created login bloc"
    This reverts commit 2399e1b2aae9cfc9ceac957f2757de6c6299e903.

--- lib/blocs/login_bloc/login_event.dart
+++
@@ -1,8 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class LoginEvent extends Equatable {
-  @override
-  List<Object> get props => null;
-}
-
-class SignInButtonPressed extends LoginEvent {}
