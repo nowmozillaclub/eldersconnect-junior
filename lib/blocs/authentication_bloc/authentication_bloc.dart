8e4914bb4e8f24840a1ac1503b970b3842891d4a
urmilshroff <urmilshroff@gmail.com>
2020-02-19 16:49:46 +0530

    Added some login logic

--- lib/blocs/authentication_bloc/authentication_bloc.dart
+++ lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:ec_junior/blocs/authentication_bloc/authentication_event.dart';
 import 'package:ec_junior/blocs/authentication_bloc/authentication_state.dart';
+import 'package:ec_junior/models/user.dart';
 import 'package:ec_junior/models/user_repository.dart';
+import 'package:ec_junior/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
@@ -21,24 +27,62 @@ class AuthenticationBloc
   @override
   Stream<AuthenticationState> mapEventToState(
       AuthenticationEvent event) async* {
-    if (event is AppStarted) {
-      try {
-        bool isFirstLaunch = _prefs.getBool('isFirstLaunch') ?? true;
-        bool isUserLoggedIn = _prefs.getString('user') ?? true;
-        if (await _userRepository.isSignedIn() && isUserLoggedIn) {
-          yield Authenticated();
-        } else if (isFirstLaunch) {
-          yield Loading();
-        } else {
-          yield Unauthenticated();
-        }
-      } catch (error) {
+    Stream<AuthenticationState> mapEventToState(
+      AuthenticationEvent event,
+    ) async* {
+      if (event is AppStarted) {
+        yield* _mapAppStartedToState();
+      } else if (event is LoggedIn) {
+        yield* _mapLoggedInToState();
+      } else if (event is LoggedOut) {
+        yield* _mapLoggedOutToState();
+      }
+    }
+  }
+
+  Stream<AuthenticationState> _mapAppStartedToState() async* {
+    try {
+      AuthService _authService = AuthService();
+      FirebaseUser _firebaseUser = await _authService.signInWithGoogle();
+      SharedPreferences _prefs;
+
+      final String _uuid = Uuid().v4();
+      final String _name = _firebaseUser.displayName;
+      final String _email = _firebaseUser.email;
+      final String _photoUrl = _firebaseUser.photoUrl;
+
+      if (_firebaseUser != null) {
+        print('Login success: $_name, $_email');
+
+        User _user = User(
+          uuid: _uuid,
+          name: _name,
+          email: _email,
+          photoUrl: _photoUrl,
+        );
+
+        _prefs.setBool('isFirstLaunch', false);
+        _prefs.setString('user', json.encode(_user));
+
+        yield Authenticated(_user);
+      } else {
+        print('Not logged in');
         yield Unauthenticated();
       }
-    } else if (event is LoggedIn) {
-      yield Authenticated();
-    } else if (event is LoggedOut) {
+    } catch (_error) {
+      print(_error);
       yield Unauthenticated();
     }
+  }
+
+  Stream<AuthenticationState> _mapLoggedInToState() async* {
+    SharedPreferences prefs;
+    final User _user = User.fromJson(json.decode(prefs.getString('user')));
+    yield Authenticated(_user);
+  }
+
+  Stream<AuthenticationState> _mapLoggedOutToState() async* {
+    _userRepository.signOut();
+    yield Unauthenticated();
   }
 }
--- lib/blocs/authentication_bloc/authentication_state.dart
+++ lib/blocs/authentication_bloc/authentication_state.dart
@@ -1,3 +1,4 @@
+import 'package:ec_junior/models/user.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class AuthenticationState extends Equatable {
@@ -7,7 +8,16 @@ abstract class AuthenticationState extends Equatable {
 
 class Uninitialized extends AuthenticationState {}
 
-class Authenticated extends AuthenticationState {}
+class Authenticated extends AuthenticationState {
+  final User _user;
+  Authenticated(this._user);
+
+  @override
+  List<Object> get props => [_user];
+
+  @override
+  String toString() => 'Authenticated $_user';
+}
 
 class Unauthenticated extends AuthenticationState {}
 
